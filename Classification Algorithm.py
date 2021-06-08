45ac0deb4dea9b5d4cd50a59171f5583df8f2545
ClearHelp Pty Ltd <techsupport@clearhelp.com.au>
2021-06-08 22:18:23 +1000

    Ver 1

---
+++ Classification Algorithm.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import urllib.request as rq
+from sklearn import preprocessing
+
+rq.urlretrieve ("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv", "ChurnData.csv")
+churn_df = pd.read_csv("ChurnData.csv")
+churn_df = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip',   'callcard', 'wireless','churn']]
+churn_df['churn'] = churn_df['churn'].astype('int')
+
+X = np.asarray(churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']])
+y = np.asarray(churn_df['churn'])
+
+X = preprocessing.StandardScaler().fit(X).transform(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
+
+yhat = LR.predict(X_test)
+yhat_prob = LR.predict_proba(X_test)
+
+from sklearn.metrics import jaccard_score
+print(jaccard_score(y_test, yhat,pos_label=0))
+
+
+
+
+
+
+
+
+
+
+
