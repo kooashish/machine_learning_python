94f25ba03378476beeb99a8616c700a7d54152a8
ClearHelp Pty Ltd <techsupport@clearhelp.com.au>
2021-06-08 19:31:32 +1000

    Error rectified and commented

--- Linear Regression.py
+++ Linear Regression.py
@@ -9,16 +9,25 @@ rq.urlretrieve("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.clo
 df = pd.read_csv("FuelConsumption.csv")
 cdf = df[['ENGINESIZE','CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
 
+# Method to split a data set in train and test
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+# Method to split a data set in test and train set
 x = cdf[['ENGINESIZE']]
 y = cdf[['CO2EMISSIONS']]
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
 regr = LinearRegression()
 regr.fit(x_train,y_train)
 
+# Not the best method as there could be a overlap but this is only for demonstration purpose
+# Best method is to use the test dataset created which is test_x and predict pred_y and compare the results
+# with y_test to show the model's efficacy
 test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
 test_y_ = regr.predict(test_x)
+
 
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
